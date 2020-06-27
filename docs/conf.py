d8d44364b6cf98ef28677337c82622bd3c7d6ad7
oursu-broad <oursu@broadinstitute.org>
2020-06-27 16:20:39 -0400

    test

--- docs/conf.py
+++ docs/conf.py
@@ -79,10 +79,6 @@ pygments_style = 'sphinx'
 todo_include_todos = False
 
 
-######### mine
-.. automodule:: io
-   :members:
-
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
