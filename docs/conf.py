a68ff6994ae55705dd930249538e92b92abbd105
oursu-broad <oursu@broadinstitute.org>
2020-06-27 16:43:07 -0400

    test

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,9 @@ needs_sphinx = '2.0'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc'
+import sphinx_rtd_theme
+
+extensions = ['sphinx.ext.autodoc',"sphinx_rtd_theme"
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -47,13 +49,6 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-import sphinx_rtd_theme
-
-extensions = [
-    ...
-    "sphinx_rtd_theme",
-]
-
 html_theme = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
